bdbce12796ef45aaefbaa13a04ca3f6a029b110c
Letícia Ribeiro <lerihribeiro@gmail.com>
2024-02-26 20:08:06 -0300

    voltando a baseURL para 'http://127.0.0.1:5000' na conexão com a api e voltando para app.run(debug=True) na api

--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://virtual-asphalt-lab-production.up.railway.app"
+    baseURL: "http://127.0.0.1:5000"
 })
 
 export default api;
