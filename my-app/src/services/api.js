db5b64b2893d1000b41eefb83f19ff8f0d53e3b4
Letícia Ribeiro <lerihribeiro@gmail.com>
2024-02-26 19:29:18 -0300

    alterando host da api

--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:5000"
+    baseURL: "http://virtual-asphalt-lab-production.up.railway.app"
 })
 
 export default api;
