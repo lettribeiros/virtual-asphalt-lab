27e7fca0226f4390a3f8cdd7808cc824d0ef02ae
Letícia Ribeiro <lerihribeiro@gmail.com>
2024-02-26 19:37:47 -0300

    alterando porta e debug na api

--- api/main.py
+++ api/main.py
@@ -161,4 +161,4 @@ def not_found():
     return jsonify({ 'message': 'Not Found' }), 404
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=True)
